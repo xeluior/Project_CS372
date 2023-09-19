1b39edfd6aa1e88384e780886b1a1d844eaa2832
Robert Gingras <gingrm02@pfw.edu>
2023-09-19 20:40:47 +0000

    Initial search route

--- server/index.js
+++ server/index.js
@@ -21,19 +21,32 @@ const port = (process.env.PORT !== undefined) ? process.env.PORT : 8080;
 // defaults or hard codes should never happen since the connection string includes the password
 // this will fail if the environment variable is not present
 const mongo = new mongodb.MongoClient(process.env.MONGO_URI);
+const media = mongo.db(db_name).collection(collection_name);
 
 // recommendation route
 // takes the "ns" and "id" query parameters to uniquely identify the media to get
 app.get("/recommendation", (req, res) => {
-    mongo.db(db_name)
-        .collection(collection_name)
-        .findOne({
-            namespace: req.query.ns,
-            id: req.query.id
-        })
-        .then((media) => {
-            res.send(media);
-        });
+    media.findOne({
+        namespace: req.query.ns,
+        id: req.query.id
+    })
+    .then((result) => {
+        res.send(result);
+    });
+});
+
+// search route
+// a "text" index needs to be made on the title field before this will work
+app.get("/search", (req, res) => {
+    media.find({
+        $text: {
+            $search: req.query.title
+        }
+    })
+    .toArray()
+    .then((result) => {
+        res.send(result);
+    });
 });
 
 // start the app
