f8010b0969c673042f773ac9f8a4c4543a04bb0d
BBergle <bennybergle@icloud.com>
2023-09-20 13:20:49 -0400

    Added login and recommend pages and also added links on the home page to access each page

---
+++ frontend/ugmra/src/pages/recommend.js
@@ -0,0 +1,10 @@
+//The recommend page for UGMRA (second screen)
+import React from "react";
+
+const Recommend = () => {
+    return (
+        <h1> Screen 3</h1>
+    )
+}
+
+export default Recommend;
