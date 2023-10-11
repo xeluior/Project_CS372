6e954acfdc9c2c87f4b333044eb0180f81d6e44d
Bobzemob <greeht01@pfw.edu>
2023-10-11 14:39:20 -0400

    Added basic filtering functionality
    - Added filterMediaNamespaces, which accepts the mediadata returned from the database and cross references it with a user-supplied list of filtered namespaces which are removed from the resulting json array.
    
    TODO: Give the user access to filters through the GUI.

--- frontend/src/pages/filter.js
+++ frontend/src/pages/filter.js
@@ -10,6 +10,48 @@ const WrapDiv = styled.div`
   flex-direction: column;
   gap: 20px;
 `
+// All media related namespaces on the tvtropes site.
+let mediaNamespaces = [
+  "Advertising",
+  "Animation",
+  "Anime",
+  "ARG",
+  "Art",
+  "AudoPlay",
+  "Blog",
+  "ComicBook",
+  "ComicStrip",
+  "Creator",
+  "Fanfic",
+  "Film",
+  "Franchise",
+  "Literature",
+  "Magazine",
+  "Manga",
+  "Manhua",
+  "Manhwa",
+  "Music",
+  "Myth",
+  "Pinball",
+  "Podcast",
+  "Radio",
+  "Ride",
+  "Roleplay",
+  "Script",
+  "Series",
+  "TabletopGame",
+  "Theatre",
+  "Toys",
+  "VideoGame",
+  "VisualNovel",
+  "WebAnimation",
+  "WebComic",
+  "Website",
+  "WebOriginal",
+  "WebVideo",
+  "WesternAnimation",
+  "Wrestling",
+]
 
 class Filter extends React.Component {
   constructor(props) {
@@ -18,6 +60,7 @@ class Filter extends React.Component {
       queryText: "",
       mediaData: null,
       checkboxData: null,
+      namespaceData: null,
     }
   }
 
@@ -41,6 +84,69 @@ class Filter extends React.Component {
       console.error("Error fetching data from the database:", error)
     }
   }
+  // Returns a json object array of all elements that do not contain any of the namespaces in namespaceList
+  filterMediaNamespaces(jsonArray, filterList) {
+    //NOTE: This list only contains namespaces that hold media, not tropes.
+    let allNameSpaces = {
+      Advertising: true,
+      Animation: true,
+      Anime: true,
+      ARG: true,
+      Art: true,
+      AudoPlay: true,
+      Blog: true,
+      ComicBook: true,
+      ComicStrip: true,
+      Creator: true,
+      Fanfic: true,
+      Film: true,
+      Franchise: true,
+      Literature: true,
+      Magazine: true,
+      Manga: true,
+      Manhua: true,
+      Manhwa: true,
+      Music: true,
+      Myth: true,
+      Pinball: true,
+      Podcast: true,
+      Radio: true,
+      Ride: true,
+      Roleplay: true,
+      Script: true,
+      Series: true,
+      TabletopGame: true,
+      Theatre: true,
+      Toys: true,
+      VideoGame: true,
+      VisualNovel: true,
+      WebAnimation: true,
+      WebComic: true,
+      Website: true,
+      WebOriginal: true,
+      WebVideo: true,
+      WesternAnimation: true,
+      Wrestling: true,
+    }
+
+    for(let i = 0; i < filterList.length; i++)
+    {
+      allNameSpaces[filterList[i]] = false // Mark all elements from filterList as false
+    }
+
+    let resultArray = []
+    let resultArrayIndex = 0
+
+    for(let i = 0; i < jsonArray.length; i++)
+    {
+      if(allNameSpaces[jsonArray[i]['ns']] === true) // If element from jsonArray is on allowed list (i.e. not in filterList)
+      {
+        resultArray[resultArrayIndex++] = jsonArray[i]
+      }
+    }
+    console.log(resultArray)
+    return resultArray
+  }
 
   render() {
     const { mediaData } = this.state
@@ -58,9 +164,9 @@ class Filter extends React.Component {
             rightContent={checkboxData}
           /> */}
           </div>
-          <div> {console.log(mediaData)}
+          <div>
             {mediaData ? (
-              <MediaGrid mediaData={mediaData} /> 
+              <MediaGrid mediaData={this.filterMediaNamespaces(mediaData, [])} />
             ) : (
               <p>Loading data...</p>
             )}
