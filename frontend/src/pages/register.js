d9c47fec915d6808898351efef4e4f8f8778c4cc
Bobzemob <greeht01@pfw.edu>
2023-10-25 13:59:31 -0400

    Added '_' to unused "isValidEmail" var in register.js

--- frontend/src/pages/register.js
+++ frontend/src/pages/register.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Container, LoginForm, Input, Button, Title } from "../styles/FormStyle";
-import { isValidEmail } from "../lib/help_lib_functions";
+import { _isValidEmail } from "../lib/help_lib_functions";
 
 export default function Register() {
     const [formData, setFormData] = useState({
