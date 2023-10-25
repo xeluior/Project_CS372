22a765572e7b1075e4b2c1c57be9621e1451f5e7
Bobzemob <greeht01@pfw.edu>
2023-10-25 14:38:55 -0400

    Removed unused var from register.js

--- frontend/src/pages/register.js
+++ frontend/src/pages/register.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Container, LoginForm, Input, Button, Title } from "../styles/FormStyle";
-import { _isValidEmail } from "../lib/help_lib_functions";
 
 export default function Register() {
     const [formData, setFormData] = useState({
