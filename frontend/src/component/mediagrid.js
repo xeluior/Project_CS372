9cb27ed392c58c0df2826691d6822b773de6d272
HenrithicusGreenson <89527529+HenrithicusGreenson@users.noreply.github.com>
2023-10-30 11:07:19 -0400

    Eslint frontend fixes (#35)
    * Added '_' to unused "isValidEmail" var in register.js
    
    * Removed unused parameter "e" from handleSubmit function in login.js
    
    * Fixed "process" being detected as undefined
    
    This was fixed by adding it to a "globals" section at the end of the eslint ignore file.
    
    * Fixed missing key prop eslint error in mediagrid.js
    
    * Added line to eslint ignore file that ignores all errors coming from the "build" directory.
    
    * Removed unused var from register.js

--- frontend/src/component/mediagrid.js
+++ frontend/src/component/mediagrid.js
@@ -8,6 +8,7 @@ const GridContainer = styled.div`
   grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
   gap: 20px;
 `
+let key = 0
 
 class MediaGrid extends Component {
   render() {
@@ -16,6 +17,7 @@ class MediaGrid extends Component {
       <GridContainer>
         {this.props.mediaData.map((mediaItem) => (
           <MediaPoster
+            key={key++}
             title={mediaItem.title}
             synopsis={mediaItem.url}
             posterUrl={"mediaItem.posterUrl"}
