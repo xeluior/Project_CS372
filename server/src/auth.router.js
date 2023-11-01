51e8e060b5558a69c69d4beded1dfbf75153026f
Robert Gingras <developer@three-point-five.dev>
2023-11-01 10:37:20 -0400

    added logout route to destroy the session and remove the session cookie

--- server/src/auth.router.js
+++ server/src/auth.router.js
@@ -1,21 +1,23 @@
 // import modules
 const express = require('express');
-const body_parser = require('body-parser');
+const body_parser = require('body-parser')
 const auth = require('./auth.js')
 
 // application constants
-const router = express.Router();
+const router = express.Router()
 
 // parse all POST requests as urlencoded
-router.use(body_parser.urlencoded());
+router.use(body_parser.urlencoded())
 
 // login endpoint returns a session id after a successful password check
-router.post("/login", auth.check_password);
+router.post("/login", auth.check_password)
 
 // create endpoint creates a new user with username and hashed password
-router.post("/create", auth.create_user);
+router.post("/create", auth.create_user)
 
 // delete endpoint deletes the user after reauthenticating them
-router.post("/delete", auth.delete_user);
+router.post("/delete", auth.delete_user)
+
+router.get("/logout", auth.logout)
 
 exports.router = router;
