bfb41dcff90f2b3116457d3aa3e8d59c2f548d5e
Bobzemob <greeht01@pfw.edu>
2023-11-21 14:39:54 -0500

    Added basic paging functionality

--- frontend/src/component/mediagrid.js
+++ frontend/src/component/mediagrid.js
@@ -1,43 +1,76 @@
 import React, { Component } from "react"
 import styled from "styled-components"
 import MediaPoster from "./mediaposter"
-import PropTypes from "prop-types";
+import PropTypes from "prop-types"
 
 const GridContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-  grid-auto-rows: 400px; /* Set a fixed height for each row, change this value as needed */
+  grid-auto-rows: 400px;
   gap: 20px;
   flex: 1;
   overflow: auto;
-`;
+`
 
 let key = 0
 
 class MediaGrid extends Component {
+  state = {
+    startIndex: 0,
+    itemsPerPage: 20,
+  }
+
+  handleNextClick = () => {
+    this.setState((prevState) => ({
+      startIndex: prevState.startIndex + prevState.itemsPerPage,
+    }))
+  }
+
+  handlePrevClick = () => {
+    this.setState((prevState) => ({
+      startIndex: Math.max(0, prevState.startIndex - prevState.itemsPerPage),
+    }))
+  }
+
   render() {
+    const { mediaData } = this.props
+    const { startIndex, itemsPerPage } = this.state
+    const visibleItems = mediaData.slice(startIndex, startIndex + itemsPerPage)
 
     return (
       <GridContainer>
-        {this.props.mediaData.map((mediaItem) => (
+        {visibleItems.map((mediaItem) => (
           <MediaPoster
             key={key++}
             title={mediaItem.title}
             synopsis={mediaItem.url}
-            posterUrl={"mediaItem.posterUrl"}
+            posterUrl={mediaItem.posterUrl} // Remove quotes around mediaItem.posterUrl
           />
         ))}
+        <div>
+          <button onClick={this.handlePrevClick} disabled={startIndex === 0}>
+            Previous
+          </button>
+          <button
+            onClick={this.handleNextClick}
+            disabled={startIndex + itemsPerPage >= mediaData.length}
+          >
+            Next
+          </button>
+        </div>
       </GridContainer>
     )
   }
 }
 
 MediaGrid.propTypes = {
-  mediaData: PropTypes.arrayOf(PropTypes.shape({
-    title: PropTypes.string,
-    url: PropTypes.string,
-    posterUrl: PropTypes.string,
-  })).isRequired, // Validate mediaData as an array of objects with specific shape
-};
+  mediaData: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      url: PropTypes.string,
+      posterUrl: PropTypes.string,
+    })
+  ).isRequired,
+}
 
 export default MediaGrid
