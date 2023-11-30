a67f03963072a56b8340cfba8137253c14ca266a
HenrithicusGreenson <greeht01@pfw.edu>
2023-11-30 13:37:44 -0500

    Centered Image

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -2,13 +2,14 @@ import React, { Component } from "react"
 import SearchBar from "../component/searchbar"
 import "../App.css"
 import StaffPicks from "../component/staffpicks"
+import logo from "./ugmra-logo.png";
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <div>
-          <img src="ugmra-logo.png" />
+        <div style={{ textAlign: "center" }}>
+          <img src={logo} alt="UGMRA Logo" style={{ margin: "auto" }} width="200" height="auto" />
         </div>
         <div className="search">
           <SearchBar />
